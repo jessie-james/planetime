bfa8d6ee0e65b9dc135bbd6b031fd00b486401cb
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-11-19 11:57:31 -0700

    index app and packages

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
