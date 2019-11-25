a6d43028b70b77125fc25694236705ed939b3204
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-11-25 10:55:01 -0700

    home and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
+import './App.css'
+import Home from './Home'
 
 const App = () => {
     return (
         <div>
-            
+            <Home/>
         </div>
     )
 }
