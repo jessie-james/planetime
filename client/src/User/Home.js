dff66f6c7e536e785afaab7f01feefc55c1360e9
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-11-26 10:59:19 -0700

    homeAndForm

--- client/src/User/Home.js
+++ client/src/User/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
         <div className='home'>
             <div className='home-signup-side'>
                 <form action="" className='home-signup'>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
+                    <label htmlFor="">Name</label><input placeholder='First Name' type="text"/>
+                    <input type="text" placeholder='Last Name'/>
+                    <label htmlFor="">Birthday</label><input type="date"/>
+                    <label htmlFor="">Start date</label><input type="date"/>
+                    <label htmlFor="">End Date</label><input type="date"/>
+                    <input type="time"/>
+                    <input type="time"/>
                 </form>
                 <div>
                     <h3>Rulez</h3>
@@ -21,6 +22,25 @@ const Home = () => {
                         <li>only Jeremy is allowed</li>
                     </ul>
                 </div>
+                <div>
+                    <h3>Specs:</h3>
+                    <ul>
+                        <li>this is a spec (missing icon)</li>
+                        <li>this is a spec (missing icon)</li>
+                        <li>this is a spec (missing icon)</li>
+                        <li>this is a spec (missing icon)</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Things to keep in mind:</h3>
+                    <ul>
+                        <li>We arent responsible for your death</li>
+                        <li>dont drink and fly</li>
+                        <li>please keep your hands and feet inside the plane at all times</li>
+                        <h4>full disclaimer can be read <a href='https://google.com'>here</a></h4>
+                    </ul>
+                </div>
+            <button>Book Now!! >> </button>
             </div>
             <div>
                 {/* res overview here */}
