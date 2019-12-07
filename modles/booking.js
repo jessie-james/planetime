ac694b79e46dc9360941d4e8bcf3480121fec4d2
Moroni Allred <moroniallred@gmail.com>
2019-12-07 13:06:50 -0700

    added scema

--- modles/booking.js
+++ modles/booking.js
@@ -2,7 +2,24 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const bookingSchems = new Schema({
-    
+    weekDay: {
+        type: String
+    },
+    month:{
+        type: String
+    },
+    day: {
+        type: String
+    },
+    year: {
+        type: String
+    }
+    // startTime:{
+
+    // },
+    // endTime:{
+
+    // }
 })
 
 module.exports = mongoose.model("Booking", bookingSchems)
