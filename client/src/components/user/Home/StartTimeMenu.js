b5ccbdf0cb9deaad013dfb52f41a4204d8e0ea87
kevin sherrell <k.sherrell.1028@gmail.com>
2020-04-23 22:11:20 -0500

    ...

--- client/src/components/user/Home/StartTimeMenu.js
+++ client/src/components/user/Home/StartTimeMenu.js
@@ -15,7 +15,7 @@ class StartTimeMenu extends React.Component {
         this.props.setTime(type, time)
         this.toggleMenu()
     }
-    toggleMenu=()=>{
+    toggleMenu = () => {
         this.setState({
             menuToggle: !this.state.menuToggle
         })
@@ -25,7 +25,8 @@ class StartTimeMenu extends React.Component {
     mapTimes = timeArr.map((time) => {
         return (
 
-                <li className={"cart__time-option-list-item"} onClick={this.handleClick} key={time.key}>{time.hours}:{time.minutes}</li>
+            <li className={"cart__time-option-list-item"} onClick={this.handleClick}
+                key={time.key}>{time.hours}:{time.minutes}</li>
 
         )
     })
@@ -36,12 +37,12 @@ class StartTimeMenu extends React.Component {
             <div className={"cart__time-block-wrapper cart__time-block-wrapper--start"}>
                 <div className={"cart__time-option-select"}>
                     <div className={`cart__time-option-selected `} onClick={this.toggleMenu}>
-                        <p className={"cart__time-option-selected-text"}>{this.state.time ? this.state.time :"start time"}</p>
+                        <p className={"cart__time-option-selected-text"}>{this.state.time ? this.state.time : "start time"}</p>
 
                         <p><span className="material-icons cart__time-option-selected-icon">arrow_drop_down</span></p>
                     </div>
                 </div>
-                <ul  className={`cart__time-option-drop-down ${!this.state.menuToggle && "cart__time-option-drop-down--closed" }`}>
+                <ul className={`cart__time-option-drop-down ${!this.state.menuToggle && "cart__time-option-drop-down--closed"}`}>
                     {this.mapTimes}
                 </ul>
             </div>
